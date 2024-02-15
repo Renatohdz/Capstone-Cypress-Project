bf3c4ed36a7128042ba97da0bf304f2da0535de3
Renato Hernandez <renato.hernandez@wizeline.com>
2024-02-14 18:44:32 -0600

    Login Update

--- cypress/e2e/SwagLabs.cy.js
+++ cypress/e2e/SwagLabs.cy.js
@@ -1,6 +1,14 @@
-describe('Swag Labs Page Login', () => {
+describe('Login Page Swag Labs', () => {
   it('passes', () => {
+    //Visit the login page
     cy.visit('https://www.saucedemo.com/')
+
+  //Add Credentials
+    cy.get([placeholder="Username"]).type(Username);
+    cy.get([placeholder="Password"]).type(Password);
+
+    //Click button Login
+    cy.get([class="submit-button btn_action"]).click();
 
   })
 })
