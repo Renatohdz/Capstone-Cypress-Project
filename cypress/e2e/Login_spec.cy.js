3c090ff0827b87a79875ce1a0614551478d571ef
Renato Hernandez <renato.hernandez@wizeline.com>
2024-02-15 10:10:34 -0600

    update login

--- cypress/e2e/Login_spec.cy.js
+++ cypress/e2e/Login_spec.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+describe('Login Page', () => {
+  it('log in successfully', () => {
+
+    //Visit the login page
+    cy.visit('https://www.saucedemo.com/'); //Open the page
+
+  //Add Credentials username and password
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+
+    //Click button Login
+    cy.get('#login-button').click(); // click in login
+
+    //Verify that the user  is logged in correctly
+    cy.url().should('include'. '/inventory.html' )
+
+    
   })
 })
