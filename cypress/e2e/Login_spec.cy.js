2300e14c4b7f954aa450b17467bf4ab85f2f0dc5
Renato Hernandez <renato.hernandez@wizeline.com>
2024-02-16 11:33:57 -0600

    Update

--- cypress/e2e/Login_spec.cy.js
+++ cypress/e2e/Login_spec.cy.js
@@ -8,11 +8,11 @@ describe('Login Page', () => {
     //cy.url().should("contain","inventory.html")
 
   //Add Credentials username and password
-    cy.get('[name="username"]').type('Admin')
-    cy.get('[name="password"]').type('admin123')
+    cy.get('[id="user-name"]').type('standard_user')
+    cy.get('[id="password"]').type('secret_sauce')
 
     //Click button Login
-    cy.get('[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click() // click in login
+    cy.get('[id="login-button"]').click() // click in login
 
     //Verify that the user  is logged in correctly
    // cy.url().should('include'. '/inventory.html' )
