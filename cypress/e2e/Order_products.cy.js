64b528604d9ea47a044dcedf676ef2607e4c5540
Renato Hernandez <renato.hernandez@wizeline.com>
2024-03-21 09:42:28 -0600

    “update”

--- cypress/e2e/Order_products.cy.js
+++ cypress/e2e/Order_products.cy.js
@@ -26,7 +26,7 @@ describe ('Order Product',() => {
         
     })
 
-    it('Product ASC to DESC',()=> {
+    it('Product Low to High',()=> {
 
         //Add Credentials username and password
       loginPage.elements.nameLabel().type('standard_user')
@@ -36,10 +36,25 @@ describe ('Order Product',() => {
     //Click button Login
       loginPage.elements.bNext().click()
 
+      //Valida que el usuario inicio correctamente   
+      cy.url().should('include', '/inventory.html') 
+
     //Order products
       loginPage.elements.containe().select('Price (low to high)')
 
+     //Valida que los productos si se hicieron de menor a mayor   
+        cy.get('[class="inventory_container"]')
+            .then(($prices)=>{
+               const prices = $prices.map((index, el) => parseFloat(el.innerText.replace('$',''))).toArray()
+               const sortedPrices = [...prices].sort((a, b) => a - b)
+               expect(prices).to.deep.equal(sortedPrices)  
 
+            })
+
+        //cy.get('class="inventory_item_name "]')
+        //cy.should('include' , '7.99')
+
+        
 
 
     })
--- cypress/support/pages/LoginPage.js
+++ cypress/support/pages/LoginPage.js
@@ -7,7 +7,7 @@ class LoginPage {
         passwordLabel: () => cy.get('[id="password"]'),
         bNext: () => cy.get('[id="login-button"]'),
         containe: ()=> cy.get('[class="product_sort_container"]')
-
+        //priceproduct: () => cy.get('[class="inventory_item_price"]')
 
 
     }
